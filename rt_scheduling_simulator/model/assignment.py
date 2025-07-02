7fb6667f9f95707f14cf9767544c5c47f7422d4f
Michael Kosco <e12124544@student.tuwien.ac.at>
2025-07-02 12:27:01 +0200

    doc: describe assignments in more detail

--- rt_scheduling_simulator/model/assignment.py
+++ rt_scheduling_simulator/model/assignment.py
@@ -7,5 +7,10 @@ from rt_scheduling_simulator.model.resource import Resource
 class Assignment:
     task_name: str
     resource_name: str
+    # the start from which execution step the resource is needed
     start: int
+    """ 
+    the last execution step for which the resource is needed, so inclusive
+    e.g. end = 5, meaning the resource is needed up until (and including) execution step 5
+    """
     end: int
