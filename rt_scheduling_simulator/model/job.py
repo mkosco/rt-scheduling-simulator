db3f01f51a8af2a28dafcdd0e0c4c5c9679b0b9e
Michael Kosco <e12124544@student.tuwien.ac.at>
2025-07-17 11:17:17 +0200

    feat: create blocked state

--- rt_scheduling_simulator/model/job.py
+++ rt_scheduling_simulator/model/job.py
@@ -5,10 +5,17 @@ from typing import Optional
 from rt_scheduling_simulator.model.resource import Resource
 
 class JobState(str, Enum):
+    # job not yet ready to be executed
     INACTIVE = "inactive"
+    # job is ready to be executed but not executing yet
     WAITING = "waiting"
+    # the job is waiting for resources
+    BLOCKED = "blocked"
+    # job is currently beeing executed
     EXECUTING = "executing"
+    # job has it's steps executed matching the execution requirenment
     FINNISHED = "finnished"
+    # the job is no longer active but has not had it's execution requirement fulfilled
     MISSED = "missed"
 
 @dataclass
