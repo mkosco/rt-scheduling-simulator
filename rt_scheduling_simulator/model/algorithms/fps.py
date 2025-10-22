4bb01077133ee6568d7bec55b5ed03a4a7c6e9f3
Michi <kosco.michael@protonmail.com>
2025-10-22 17:56:57 +0200

    feat: implement PIP

--- rt_scheduling_simulator/model/algorithms/fps.py
+++ rt_scheduling_simulator/model/algorithms/fps.py
@@ -1,3 +1,4 @@
+from queue import Empty
 from rt_scheduling_simulator.model.algorithms.algorithm import Algorithm 
 from rt_scheduling_simulator.model.job import Job, JobState
 from rt_scheduling_simulator.logging import debug_print
@@ -8,33 +9,36 @@ class FPS(Algorithm):
         debug_print(f"\nFPS")
         super().summarize()
         pass
+    
+    # Use a helper to treat None priorities as very low priority (0)
+    def effective_priority(self, job: Job) -> int:
+        # If either priority is None, treat it as low priority (0).
+        fps = job.fps_priority if job.fps_priority is not None else 0
+        proto = job.protocol_priority if job.protocol_priority is not None else 0
+        return int(max(fps, proto))
 
     """ 
     This is FPS so we sort according to the highest priority 
     One is the highets priority, higher integers mean a lower priority
     """
     def sort_jobs(self) -> list[Job]:
-        # TODO adjust this so it takes whatever is bigger fps_priority or protocol priority
-        return [job for job in sorted(self.active_jobs, key=lambda job: job.fps_priority)]
+        return [job for job in sorted(self.active_jobs, key=self.effective_priority, reverse=True)]
     
     def upate_resource_assignments(self, sorted_jobs) -> None:
         super().upate_resource_assignments(sorted_jobs)
-    
-        # this is the pip workflow 
         
         # should pip be active 
-        if self.protocol is Protocol.PIP:
+        if self.protocol == Protocol.PIP:
             # find jobs that have a locked resource that is needed by a higher prio job
             # check blocked jobs 
             blocked_jobs = [job for job in self.active_jobs if job.state is JobState.BLOCKED]
             for job in blocked_jobs:
-                if job.resources_needed is not None:
-                    
+                if job.resources_needed is not None and job.resources_needed:
                     # get a list of jobs that hold resources the current job needs
                     jobs_holding_needed_resources: list[Job] = []
                     for resource in job.resources_needed:
                         job_with_resource = self.resource_to_job.get(resource.name)
-                        if job_with_resource is not None:
+                        if job_with_resource is not None and job_with_resource is not job:
                             jobs_holding_needed_resources.append(job_with_resource)
                     
                     """
@@ -42,9 +46,5 @@ class FPS(Algorithm):
                     if the currently blocked job has a higher prioritys
                     """
                     for job_holding_needed_resource in jobs_holding_needed_resources:
-                        if max(job_holding_needed_resource.fps_priority, job_holding_needed_resource.protocol_priority) < max(job.protocol_priority, job.fps_priority):
-                            job_holding_needed_resource.protocol_priority = job.fps_priority
-                    
-                # check wether there is a high prio job that needs a resource of a lower prio job 
-                
-                # if that is the case increase the prio of the lower prio job
+                        if self.effective_priority(job_holding_needed_resource) < self.effective_priority(job):
+                            job_holding_needed_resource.protocol_priority = job.fps_priority if job.fps_priority is not None else job.protocol_priority
